41ab2014a9614f240388b1fe2f9dd28bc5ca002a
troutoune <thms.ths@gmail.com>
2014-05-20 13:20:57 +0200

    Added a few things, class should be operationnal. Need to add some methods.

--- Node.java
+++ Node.java
@@ -1,9 +1,28 @@
-public class Node
-{
-	//Mettre une ArrayList de Links.
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import java.lang.Math;
 
-	Node()
+public class Node extends JPanel
+{
+	ArrayList<Link> LinkList = new ArrayList<Link>();
+
+	int ID;
+	int X;
+	int Y;
+
+	Node(int X, int Y, int ID)
 	{
-	
+		this.X = X;
+		this.Y = Y;
+		this.ID =ID;
 	}
+
+	public void paint(Graphics g)
+	{
+		g.fillOval(X - 5, Y - 5, 10, 10);
+		g.drawString(Integer.toString(ID), X + 5, Y + 5);
+	}
+
 }
