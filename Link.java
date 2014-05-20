2bfce226896fcc9314143ddeaccea8b72f866335
troutoune <thms.ths@gmail.com>
2014-05-20 13:20:07 +0200

    Added an inheritance from JPanel.

--- Link.java
+++ Link.java
@@ -1,11 +1,22 @@
-public class Link
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import java.lang.Math;
+
+public class Link extends JPanel
 {
 	Node from;
 	Node to;
 
 	Link(Node from, Node to)
 	{
-		this.from = From;
+		this.from = from;
 		this.to = to;
+	}
+
+	public void paint(Graphics g)
+	{
+
 	}
 }
